01e5d83cb55a0be6fabff775ddda5aa02631ed08
KonstantinPetrov21 <konstantin.petrov.12@inbox.ru>
2025-02-27 17:12:37 +0300

    ппа

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Advantage from '@/view/sectionOne/Advantage';
 import PreviewSection from "@/view/PreviewSection/PreviewSection";
 
 export default function Home() {
   return (
     <div>
       <PreviewSection />
+      <Advantage />
     </div>
   );
 }
