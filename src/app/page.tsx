1ea257ba808dd0f3acc9dd3498a51718e72ae1f6
Aleksandr <itprogram62@gmail.com>
2025-03-01 19:19:28 +0300

    price desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import MyLayoutContainer from "@/components/MyLayoutContainer/MyLayoutContainer"
 import About from "@/view/About/About";
 import Multifunctionality from "@/view/Multifunctionality/Multifunctionality";
 import PreviewSection from "@/view/PreviewSection/PreviewSection";
+import Price from "@/view/Price/Price";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <MyLayoutContainer>
         <About />
         <Multifunctionality />
+        <Price />
       </MyLayoutContainer>
     </div>
   );
